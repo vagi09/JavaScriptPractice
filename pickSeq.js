f09ace6f9326463929c4e1cdaf7e8dcb65cdb75e
vagi09 <m.vageeshjajur@gmail.com>
2022-05-11 12:57:25 +0530

    pick array

---
+++ pickSeq.js
@@ -0,0 +1,14 @@
+let pick = ["referred_by", "referred_to"]
+
+// consolpick[0]
+// pick[1]s
+
+// console.log(pick[0])
+// console.log(pick[1])
+
+//let rev = pick.reverse()
+
+for (let i = 0; i < pick.length; i++) {
+
+  console.log(pick[i])
+}
